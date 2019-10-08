4324755e7a0df4e884dcddfb43ff451c833ba802
Colin Maudry <colin.maudry@data.gouv.fr>
2019-10-08 19:30:24 +0200

    Updated config with actual data

--- scripts/jq/ocds/config.sh
+++ scripts/jq/ocds/config.sh
@@ -5,7 +5,7 @@ then
  export datetime=`date "+%FT%T+02:00"`
 fi
 
-export dataset_id="5d1a216e6f4441513e89b93e"
-export package_uri="https://www.data.gouv.fr/fr/datasets/r/2a84ffc7-7121-48d2-a28f-85d835ed09a4"
+export dataset_id="5cd57bf68b4c4179299eb0e9"
+export package_uri="https://www.data.gouv.fr/fr/datasets/r/68bd2001-3420-4d94-bc49-c90878df322c"
 export dataset_url="https://www.data.gouv.fr/fr/datasets/$dataset_id"
 export ocid_prefix="ocds-78apv2"
