89a8670690ef136c6206011e0a7fa1a5f7af928f
Colin Maudry <colin.maudry@data.gouv.fr>
2019-10-11 16:56:04 +0200

    Possibility to execute a single test #1

--- scripts/jq/ocds/tests/test.sh
+++ scripts/jq/ocds/tests/test.sh
@@ -3,6 +3,14 @@ cd ../../../
 
 export DECP_HOME=`pwd`
 
+function test {
+    file=$1
+
+    echo "$file"
+    $DECP_HOME/scripts/makeOCDS_json.sh $DECP_HOME/scripts/jq/ocds/tests/source/$file > result
+    diff result $DECP_HOME/scripts/jq/ocds/tests/expectedResult/$file
+}
+
 export datetime=2019-01-01T01:01:01+02:00
 export dataset_id="5d1a216e6f4441513e89b93e"
 export package_uri="https://www.data.gouv.fr/fr/datasets/r/2a84ffc7-7121-48d2-a28f-85d835ed09a4"
@@ -11,16 +19,19 @@ export ocid_prefix="ocds-78apv2"
 
 echo ""
 
-for test in `ls $DECP_HOME/scripts/jq/ocds/tests/source`
-do
-    if [[ ! -d $test ]]
-    then
-        echo "$test"
-        $DECP_HOME/scripts/makeOCDS_json.sh $DECP_HOME/scripts/jq/ocds/tests/source/$test > result
-        diff result $DECP_HOME/scripts/jq/ocds/tests/expectedResult/$test
-        echo ""
-        echo "*******************"
-    fi
-done
+if [[ -f $DECP_HOME/scripts/jq/ocds/$1 ]]
+then
+    test=$(basename $1)
+    test $test
 
+else
+    for test in `ls $DECP_HOME/scripts/jq/ocds/tests/source`
+    do
+        if [[ ! -d $test ]]
+        then
+            test $test
+        fi
+    done
+fi
 export datetime=
+rm result
